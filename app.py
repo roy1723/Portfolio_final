39b6fa23d224896f26a7ac8380bd355bb466f0ae
Sahil Banerjee <135624179+roy1723@users.noreply.github.com>
2025-02-04 01:10:08 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.secret_key = os.urandom(24)
 
 # Flask-Mail Configuration
 app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
-app.config['MAIL_PORT'] = 587
+app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
