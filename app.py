6b7b1f4a0506a60ccf7e7b0cd51f9635467fa490
Sahil Banerjee <135624179+roy1723@users.noreply.github.com>
2025-02-04 01:01:14 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ from flask import Flask, render_template, request, redirect, flash
 from flask_mail import Mail, Message
 from dotenv import load_dotenv
 import os
-load_dotenv()
+
 app = Flask(__name__)
+
+# Load environment variables
+load_dotenv()
+
+# Flask Configuration
 app.secret_key = os.urandom(24)
 
 # Flask-Mail Configuration
@@ -17,9 +22,11 @@ app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER')
 
 mail = Mail(app)
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 @app.route("/submit-form", methods=["POST"])
 def contact():
@@ -28,32 +35,36 @@ def contact():
     message = request.form.get("message")
     subject = request.form.get("subject")
     phone = request.form.get("phone")
+
+    # Validate form data
     if not name or not email or not message or not subject or not phone:
         flash("All fields are required. Please fill in all the fields before submitting.", "danger")
         return redirect('/#contact')
-    else:
-        # Compose Email
-        msg = Message(f'New Contact Form (Portfolio): {subject}',
-                      sender=email,
-                      recipients=["sahilbanerjee51@gmail.com"])
 
-        msg.body = f"""
-                Name: {name}
-                Email: {email}
-                Phone: {phone}
-                Subject: {subject}
-    
-                Message:
-                {message}
-                """
-        try:
-            mail.send(msg)  # Directly send email using Flask-Mail
-            flash("Your message has been sent successfully!", "success")
-        except Exception as e:
-            flash("Error sending message. Please try again later.", "danger")
-            print(f"Email Error: {e}")  # Debugging
+    # Compose Email
+    msg = Message(f'New Contact Form (Portfolio): {subject}',
+                  sender=email,
+                  recipients=["sahilbanerjee51@gmail.com"])
 
-        return redirect("/")
+    msg.body = f"""
+            Name: {name}
+            Email: {email}
+            Phone: {phone}
+            Subject: {subject}
+
+            Message:
+            {message}
+            """
+
+    try:
+        mail.send(msg)  # Directly send email using Flask-Mail (no need to call connect())
+        flash("Your message has been sent successfully!", "success")
+    except Exception as e:
+        flash("Error sending message. Please try again later.", "danger")
+        print(f"Email Error: {e}")  # Log the error for debugging
+
+    return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
