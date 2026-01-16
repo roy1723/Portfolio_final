bdf6835ce677073e079a87e0853beae817789936
Sahil Banerjee <135624179+roy1723@users.noreply.github.com>
2026-01-16 22:47:05 +0530

    Implement form submission to Formspree
    Add form submission handling with Formspree URL.

--- static/js/script.js
+++ static/js/script.js
@@ -12,3 +12,12 @@ window.addEventListener('load', () => {
 
     },  1200);
 })
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("contactForm");
+
+    // Replace this with your environment variable
+    const formspreeUrl = process.env.FORMSPREE_URL;
+
+    form.action = formspreeUrl;
+});
